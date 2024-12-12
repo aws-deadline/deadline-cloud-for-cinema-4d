d1d2a5841213b0a1b8cbfad77979e6eaefef36b8
Alex Hughes <ahughesalex@gmail.com>
2024-12-12 10:46:01 -0800

    Add logic to pathmap all assets in scene
    Using `paramId` instead of hardcoding `c4d.BITMAPSHADER_FILENAME` makes it work with more asset types that may be linked to other attributes.
    
    This `paramId` references what parameter the asset is assigned to within it's owner
    
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- src/deadline/cinema4d_adaptor/Cinema4DClient/cinema4d_handler.py
+++ src/deadline/cinema4d_adaptor/Cinema4DClient/cinema4d_handler.py
@@ -50,6 +50,17 @@ class Cinema4DHandler:
         self.take = "Main"
         self.map_path = map_path
 
+    def _remap_assets(self) -> None:
+        asset_list: list[Dict[str, Any]] = []
+        c4d.documents.GetAllAssetsNew(
+            self.doc, allowDialogs=False, lastPath="", assetList=asset_list
+        )
+        for asset in asset_list:
+            asset_owner = asset.get("owner")
+            asset_param_id = asset.get("paramId")
+            if asset_owner and asset_param_id:
+                asset_owner[asset_param_id] = self.map_path(asset.get("filename", ""))
+
     def start_render(self, data: dict) -> None:
         self.doc = c4d.documents.GetActiveDocument()
         self.render_data = self.doc.GetActiveRenderData()
@@ -69,6 +80,8 @@ class Cinema4DHandler:
             self.render_data[c4d.RDATA_MULTIPASS_FILENAME] = self.map_path(
                 self.render_data[c4d.RDATA_MULTIPASS_FILENAME]
             )
+
+        self._remap_assets()
 
         bm = bitmaps.MultipassBitmap(
             int(self.render_data[c4d.RDATA_XRES]),
