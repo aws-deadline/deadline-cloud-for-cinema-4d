97f2b9fe78c90aa81f2f3ea965e046833443d121
Joel Wong <127782171+joel-wong-aws@users.noreply.github.com>
2025-01-16 10:35:00 -0600

    fix: improve error handling for sticky settings having a long path
    Signed-off-by: Joel Wong <127782171+joel-wong-aws@users.noreply.github.com>

--- src/deadline/cinema4d_submitter/data_classes.py
+++ src/deadline/cinema4d_submitter/data_classes.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 
 import dataclasses
 import json
+import logging
+import traceback
 from dataclasses import dataclass, field
 from pathlib import Path
 
 from .takes import TakeSelection  # type: ignore
 
 RENDER_SUBMITTER_SETTINGS_FILE_EXT = ".deadline_render_settings.json"
+
+_logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -58,21 +62,32 @@ class RenderSubmitterUISettings:
             except (OSError, json.JSONDecodeError):
                 # If something bad happened to the sticky settings file,
                 # just use the defaults instead of producing an error.
-                import traceback
-
                 traceback.print_exc()
-                print(
-                    f"WARNING: Failed to load sticky settings file {sticky_settings_filename}, reverting to the default settings."
+                _logger.warning(
+                    f"Failed to load sticky settings file {sticky_settings_filename.absolute()}, reverting to the"
+                    + "default settings."
                 )
 
     def save_sticky_settings(self, scene_filename: str):
         sticky_settings_filename = Path(scene_filename).with_suffix(
             RENDER_SUBMITTER_SETTINGS_FILE_EXT
         )
-        with open(sticky_settings_filename, "w", encoding="utf8") as fh:
-            obj = {
-                field.name: getattr(self, field.name)
-                for field in dataclasses.fields(self)
-                if field.metadata.get("sticky")
-            }
-            json.dump(obj, fh, indent=1)
+        sticky_settings_path = str(sticky_settings_filename.absolute())
+        try:
+            with open(sticky_settings_filename, "w", encoding="utf8") as fh:
+                obj = {
+                    field.name: getattr(self, field.name)
+                    for field in dataclasses.fields(self)
+                    if field.metadata.get("sticky")
+                }
+                json.dump(obj, fh, indent=1)
+        except OSError as e:
+            traceback.print_exc()
+            if len(sticky_settings_path) >= 256:
+                # raise an error here because if we ignore this error, there will likely be later errors in rendering
+                # due to exceeding the max path length
+                raise RuntimeError(
+                    "Failed to save sticky settings file. This usually occurs from exceeding the maximum path length. "
+                    + f"Please reduce the length of your .c4d filename. Error: {e}"
+                )
+            _logger.warning(f"Failed to save sticky settings file to {sticky_settings_path}.")
