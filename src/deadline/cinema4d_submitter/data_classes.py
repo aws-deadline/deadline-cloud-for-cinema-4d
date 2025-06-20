7964ae742afac76cbae4093de7a116896c9f66d8
Lagos Gonzales <214008711+gianelli-lagos-aws@users.noreply.github.com>
2025-06-20 09:37:07 -0700

    refactor: Changed max_worker_count default and removed handling of it in save_sticky_settings
    Signed-off-by: Lagos Gonzales <214008711+gianelli-lagos-aws@users.noreply.github.com>

--- src/deadline/cinema4d_submitter/data_classes.py
+++ src/deadline/cinema4d_submitter/data_classes.py
@@ -55,7 +55,9 @@ class RenderSubmitterUISettings:
     max_failed_tasks_count: int = field(default=20, metadata={"sticky": True})
     max_retries_per_task: int = field(default=5, metadata={"sticky": True})
     limited_max_worker_count: bool = field(default=False, metadata={"sticky": True})
-    max_worker_count: int = field(default=1, metadata={"sticky": True})
+    max_worker_count: int = field(
+        default=-1, metadata={"sticky": True}
+    )  # -1 indicates no limited max worker count
 
     override_frame_range: bool = field(default=False, metadata={"sticky": True})
     override_output_path: bool = field(default=False, metadata={"sticky": True})
@@ -128,8 +130,6 @@ class RenderSubmitterUISettings:
                     obj["output_path"] = self.output_path
                 if self.override_multi_pass_path:
                     obj["multi_pass_path"] = self.multi_pass_path
-                if not self.limited_max_worker_count:
-                    obj["max_worker_count"] = -1
                 json.dump(obj, fh, indent=1)
         except OSError as e:
             traceback.print_exc()
