65a430f96c976dc46bdcf519abbce70f67a4f997
Joel Wong <127782171+joel-wong-aws@users.noreply.github.com>
2025-08-12 09:59:12 -0500

    chore: update progress reporting text to remove duplicate colon, make words lowercase
    Signed-off-by: Joel Wong <127782171+joel-wong-aws@users.noreply.github.com>

--- src/deadline/cinema4d_adaptor/Cinema4DClient/cinema4d_handler.py
+++ src/deadline/cinema4d_adaptor/Cinema4DClient/cinema4d_handler.py
@@ -34,19 +34,19 @@ def progress_callback(progress_percent, progress_type_int):
         progress_type (c4d.RENDERPROGRESSTYPE): The Main part of the current rendering step
     """
     progress_type_map = {
-        c4d.RENDERPROGRESSTYPE_BEFORERENDERING: "Before Rendering",
-        c4d.RENDERPROGRESSTYPE_DURINGRENDERING: "During Rendering",
-        c4d.RENDERPROGRESSTYPE_AFTERRENDERING: "After Rendering",
-        c4d.RENDERPROGRESSTYPE_GLOBALILLUMINATION: "Global Illumination",
-        c4d.RENDERPROGRESSTYPE_QUICK_PREVIEW: "Quick Preview",
-        c4d.RENDERPROGRESSTYPE_AMBIENTOCCLUSION: "Ambient Occlusion",
+        c4d.RENDERPROGRESSTYPE_BEFORERENDERING: "before rendering",
+        c4d.RENDERPROGRESSTYPE_DURINGRENDERING: "during rendering",
+        c4d.RENDERPROGRESSTYPE_AFTERRENDERING: "after rendering",
+        c4d.RENDERPROGRESSTYPE_GLOBALILLUMINATION: "global illumination",
+        c4d.RENDERPROGRESSTYPE_QUICK_PREVIEW: "quick preview",
+        c4d.RENDERPROGRESSTYPE_AMBIENTOCCLUSION: "ambient occlusion",
     }
     if progress_type_int in progress_type_map:
         progress_type_text = progress_type_map[progress_type_int]
     else:
         progress_type_text = f"Unknown progress type ({progress_type_int})"
 
-    print(f"Progress Update: ({progress_type_text}): {progress_percent * 100.0}%")
+    print(f"Progress update ({progress_type_text}): {progress_percent * 100.0}%")
 
     if progress_type_int == c4d.RENDERPROGRESSTYPE_DURINGRENDERING:
         print("ALF_PROGRESS %g" % (progress_percent * 100))
