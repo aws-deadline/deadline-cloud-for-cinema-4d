ea47b663e88da8b5d039e39395a3968d59642ef6
Vaishnavi Devi Gujjari <242289011+vaishhg@users.noreply.github.com>
2025-11-19 14:30:21 -0800

    chore: remove duplicate scripts
    Signed-off-by: Vaishnavi Devi Gujjari <242289011+vaishhg@users.noreply.github.com>

--- src/deadline/cinema4d_submitter/assets.py
+++ src/deadline/cinema4d_submitter/assets.py
@@ -27,9 +27,6 @@ class AssetIntrospector:
         # Clear any previous warnings before processing
         warning_collector.clear_warnings()
 
-        # Clear any previous warnings before processing
-        warning_collector.clear_warnings()
-
         # Grab tx files (if we need to)
         assets: set[Path] = set()
 
